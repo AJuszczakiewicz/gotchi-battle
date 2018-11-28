99fdde3d7748e4ccfd16434e7faf9dc411dff97d
AJuszczakiewicz <ajuszczakiewicz@protonmail.com>
2018-11-28 03:41:38 +0100

    Add Player class skeleton.

---
+++ src/main/java/Player.java
@@ -0,0 +1,12 @@
+public class Player {
+    String nickname;
+    String id;
+
+    public Player(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
